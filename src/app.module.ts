663325621a9fc46e012f051958aedd44d16c90ab
franco22bellido <franco.bellido22@gmail.com>
2024-02-20 22:43:11 -0300

    commit two: connecting to database and generating guests module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Guest } from './guests/entities/guest.entity';
+import { GuestsModule } from './guests/guests.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: "mysql",
+      host: "localhost",
+      database: "guests_party",
+      port: 3306,
+      username: "root",
+      password: "Ironman312345aAA",
+      entities: [Guest],
+      synchronize: true,
+    }),
+    GuestsModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/guests/entities/guest.entity.ts
@@ -0,0 +1,29 @@
+import {
+    Entity,
+    PrimaryGeneratedColumn,
+    Column,
+    CreateDateColumn,
+    UpdateDateColumn
+} from 'typeorm'
+
+@Entity()
+export class Guest {
+
+    @PrimaryGeneratedColumn("increment")
+    id: number;
+
+    @Column({ type: 'varchar', length: 255 })
+    firstName: string;
+
+    @Column({ type: 'varchar', length: 255 })
+    lastName: string;
+
+    @Column({ type: 'boolean', default: false })
+    hasArrived: boolean;
+
+    @CreateDateColumn()
+    createdAt: Date;
+
+    @UpdateDateColumn()
+    updatedAt: Date;
+}
